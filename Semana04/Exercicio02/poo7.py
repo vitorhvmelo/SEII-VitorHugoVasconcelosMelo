6b006fb7e2a649567dbf461e1defd18ab7590088
Vitor Hugo Vasconcelos de Melo <vitorhvmelo@gmail.com>
2022-10-29 21:30:13 -0300

    poo8

---
+++ Semana04/Exercicio02/poo7.py
@@ -0,0 +1,19 @@
+#poo7
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def get_name(self):
+        return self.name
+    
+    def get_age(self):
+        return self.age
+    
+    def set_age(self, age):
+        self.age = age
+
+
+dogs_name = ['Tim', 'Bill']
+dogs_name = [32, 14]
